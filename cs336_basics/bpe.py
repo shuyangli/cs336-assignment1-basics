d34c8578926b3998e9ac4d4ca335833f9f24855c
Shuyang Li <shuyang.li.95@gmail.com>
2025-07-21 16:39:44 -0400

    Only compute merge on each pretoken once

--- cs336_basics/bpe.py
+++ cs336_basics/bpe.py
@@ -5,6 +5,8 @@ from dataclasses import dataclass
 from collections import defaultdict
 from collections.abc import Sequence
 
+from .pretokenization_example import find_chunk_boundaries
+
 
 # GPT-2 pre-tokenization pattern
 PRETOKENIZATION_PATTERN = re.compile(r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
@@ -46,61 +48,77 @@ class Vocabulary:
         return self._add_token(new_bytes)
 
 
-def compute_next_merge(tokens: Sequence[Sequence[bytes]]) -> tuple[bytes, bytes]:
+# This is a hot spot:
+def compute_next_merge(tokens: dict[Sequence[bytes], int]) -> tuple[bytes, bytes]:
     # Compute token statistics
     token_stats: dict[tuple[bytes, bytes], int] = defaultdict(lambda: 0)
 
-    for word in tokens:
+    for word, count in tokens.items():
         for i in range(len(word) - 1):
-            token_stats[(word[i], word[i + 1])] += 1
+            token_stats[(word[i], word[i + 1])] += count
 
     # Merge tokens
-    stats_by_count: list[tuple[tuple[bytes, bytes], int]] = [(k, v) for k, v in token_stats.items()]
-    stats_by_count.sort(key=lambda t: (t[1], t[0]), reverse=True)
-    return stats_by_count[0][0]
+    stats_by_count: list[tuple[int, tuple[bytes, bytes]]] = [(v, k) for k, v in token_stats.items()]
+    stats_by_count.sort(reverse=True)
+    return stats_by_count[0][1]
 
 def train_bpe_with_text(corpus: str, vocab_size: int, special_tokens: list[str]):
     vocab = Vocabulary(vocab_size=vocab_size, special_tokens=special_tokens)
 
-    all_tokens: list[list[bytes]] = []
-
     # First split the corpus with special tokens
     split_special_token_pattern = "|".join([re.escape(token) for token in special_tokens])
+
+    # We only need to pretokenize each word once, then multiply by count
+    pretokenized_words_to_count: dict[Sequence[bytes], int] = defaultdict(int)
 
     for corpus_chunk in re.split(split_special_token_pattern, corpus):
         # Pretokenize
         for word in re.finditer(PRETOKENIZATION_PATTERN, corpus_chunk):
-            word_tokens = []
-            word_bytes = word.group(0).encode("utf-8")
-            for b in word_bytes:
-                word_tokens.append(bytes([b]))
-            all_tokens.append(word_tokens)
+            # TODO: why is this so annoying to use?
+            word_key = tuple([bytes([c]) for c in word.group(0).encode("utf-8")])
+            pretokenized_words_to_count[word_key] += 1
 
     # BPE training loop
     while len(vocab.token_to_bytes) < vocab_size:
-        next_merge = compute_next_merge(all_tokens)
+        next_merge = compute_next_merge(pretokenized_words_to_count)
         vocab.merge_tokens(next_merge)
 
-        new_all_tokens = []
-        for word in all_tokens:
-            word_tokens = []
+        new_pretokenized_words_to_count = {}
+        for word, count in pretokenized_words_to_count.items():
+            new_word = []
             i = 0
+
+            # TODO this is wrong: we are not separating the tokens correctly
             while i <= len(word) - 1:
                 if i == len(word) - 1:
-                    word_tokens.append(word[i])
+                    new_word.append(word[i])
                     i += 1
                 elif (word[i], word[i + 1]) == next_merge:
-                    word_tokens.append(word[i] + word[i + 1])
+                    new_word.append(word[i] + word[i + 1])
                     i += 2
                 else:
-                    word_tokens.append(word[i])
+                    new_word.append(word[i])
                     i += 1
-            new_all_tokens.append(word_tokens)
-        all_tokens = new_all_tokens
-        del new_all_tokens
+            new_pretokenized_words_to_count[tuple(new_word)] = count
+
+        pretokenized_words_to_count = new_pretokenized_words_to_count
+        del new_pretokenized_words_to_count
     return vocab
 
-def train_bpe(input_path: str | os.PathLike, vocab_size: int, special_tokens: list[str]) -> Vocabulary:
+def train_bpe(input_path: str | os.PathLike, vocab_size: int, special_tokens: list[str], num_processes: int = 4) -> Vocabulary:
+    with open(input_path, "rb") as f:
+        boundaries = find_chunk_boundaries(
+            f, num_processes, "<|endoftext|>".encode("utf-8"))
+
+        # The following is a serial implementation, but you can parallelize this
+        # by sending each start/end pair to a set of processes.
+        for start, end in zip(boundaries[:-1], boundaries[1:]):
+            f.seek(start)
+            chunk = f.read(end - start).decode("utf-8", errors="ignore")
+            # Run pre-tokenization on your chunk and store the counts for each pre-token
+
+
+
     with open(input_path, "r", encoding="utf-8") as f:
         corpus = f.read()
 
